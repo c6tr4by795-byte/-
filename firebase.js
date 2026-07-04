22fc88a66bddc10011129a4083559485912a0339
c6tr4by795-byte <c6tr4by795@privaterelay.appleid.com>
2026-07-04 13:30:43 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,6 @@
 // ===============================
-// FIREBASE CONFIG
+// IRAQ GREEN
+// FIREBASE
 // ===============================
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
@@ -8,22 +9,24 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth
 
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
-// بيانات مشروع IRAQ GREEN
 const firebaseConfig = {
-  apiKey: "AIzaSyCPP4Qv_oftFJ0xoj6_-pdpT0eXCgcD2aI",
-  authDomain: "hmjat187.firebaseapp.com",
-  projectId: "hmjat187",
-  storageBucket: "hmjat187.firebasestorage.app",
-  messagingSenderId: "527864029743",
-  appId: "1:527864029743:web:bf6285928ec2f77412dcda",
-  measurementId: "G-3VSYZ2J5VQ"
+
+apiKey:"AIzaSyCPP4Qv_oftFJ0xoj6_-pdpT0eXCgcD2aI",
+
+authDomain:"hmjat187.firebaseapp.com",
+
+projectId:"hmjat187",
+
+storageBucket:"hmjat187.firebasestorage.app",
+
+messagingSenderId:"527864029743",
+
+appId:"1:527864029743:web:bf6285928ec2f77412dcda"
+
 };
 
-// تشغيل Firebase
-const app = initializeApp(firebaseConfig);
+const app=initializeApp(firebaseConfig);
 
-// الخدمات
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const auth=getAuth(app);
 
-export default app;
+export const db=getFirestore(app);
